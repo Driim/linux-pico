09f1157fd04a5edd2c4aaaa463fdec2879336836
Guido Günther <agx@sigxcpu.org>
2018-08-02 19:46:21 +0200

    imx-drm: use new for_*_each_*_in_state iterators

--- drivers/gpu/drm/imx/dcss/dcss-kms.c
+++ drivers/gpu/drm/imx/dcss/dcss-kms.c
@@ -64,7 +64,7 @@ static int dcss_drm_atomic_commit(struct drm_device *drm,
 	 * If the plane fb has an dma-buf attached, fish out the exclusive
 	 * fence for the atomic helper to wait on.
 	 */
-	for_each_plane_in_state(state, plane, plane_state, i) {
+	for_each_plane_in_new_state(state, plane, plane_state, i) {
 		if ((plane->state->fb != plane_state->fb) && plane_state->fb) {
 			dma_buf = drm_fb_cma_get_gem_obj(plane_state->fb,
 							 0)->base.dma_buf;
@@ -86,7 +86,7 @@ static void dcss_kms_setup_output_pipe(struct drm_atomic_state *state)
 	struct drm_display_info *di;
 	int i;
 
-	for_each_connector_in_state(state, connector, conn_state, i) {
+	for_each_connector_in_new_state(state, connector, conn_state, i) {
 		if (!connector->state->best_encoder)
 			continue;
 
